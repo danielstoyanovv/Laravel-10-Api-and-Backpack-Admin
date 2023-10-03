bb412ff73ba2071d0ce807c7b700a8b2adafd672
Daniel Stoyanov <daniel.stoyanovv@gmail.com>
2023-10-03 16:32:58 +0300

    Update PostRepository.php

--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -2,15 +2,16 @@
 
 namespace App\Repositories;
 
+use App\Models\User;
 use Database\Factories\PostFactory;
 
 class PostRepository
 {
     /**
-     * @param $user
+     * @param User $user
      * @return mixed
      */
-    public function create($user): mixed
+    public function create(User $user): mixed
     {
         return  PostFactory::new([
             'author' => request('author'),
